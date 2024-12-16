97f76ac0174b9881e014bbe071f6ac768f16d40b
mafifi96 <mafifi350@gmail.com>
2024-12-16 14:46:48 +0200

    departments full crud

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\DepartmentRequest;
+use App\Http\Requests\Departments\DepartmentRequest;
 use App\Http\Resources\Departments\DepartmentsCollection;
 use App\Http\Resources\Departments\DepartmentsResource;
 use App\Models\Department;
@@ -14,25 +14,31 @@ class DepartmentController extends Controller
     public function index(Request $request)
     {
 
-       $departments = Department::withCount('employees')->paginate($request->limit ?? 10);
+       $departments = Department::withCount(['employees','positions'])->paginate($request->limit ?? 10);
 
        return successResponse(new DepartmentsCollection($departments));
-       return successResponse(DepartmentsResource::collection($departments));
 
     }
 
     public function show(Department $department)
     {
-        return simpleSuccessResponse(new DepartmentsResource($department));
+        return simpleSuccessResponse(new DepartmentsResource($department->load(['employees','positions'])->loadCount(['employees','positions'])));
     }
 
     public function store(DepartmentRequest $request)
     {
 
-        Department::updateOrCreate($request->validated());
+        Department::create($request->validated());
 
         return simpleSuccessResponse(message: 'department created successfully');
 
+    }
+
+    public function update(DepartmentRequest $request , Department $department)
+    {
+        $department->update($request->validated());
+
+        return simpleSuccessResponse(message: 'department updated successfully');
     }
 
     public function destroy(Department $department)
@@ -42,5 +48,5 @@ class DepartmentController extends Controller
         return simpleSuccessResponse(message: 'department deleted successfully');
 
     }
-    
+
 }
